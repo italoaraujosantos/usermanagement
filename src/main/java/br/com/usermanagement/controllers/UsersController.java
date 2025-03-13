69af891a7396b622d5b05d9dd7887b230f7f7f8d
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2025-03-13 13:13:27 -0300

    Concluidas as operações

--- src/main/java/br/com/usermanagement/controllers/UsersController.java
+++ src/main/java/br/com/usermanagement/controllers/UsersController.java
@@ -4,12 +4,21 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.usermanagement.dto.UsersDTO;
+import br.com.usermanagement.entities.Users;
 import br.com.usermanagement.services.UsersService;
 
 @RestController
@@ -18,6 +27,21 @@ public class UsersController {
 
 	@Autowired
 	private UsersService usersService;
+	
+	@PostMapping
+	ResponseEntity<List<UsersDTO>> create(@Validated @RequestBody Users user) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usersService.create(user));
+	}
+	
+	@PutMapping
+	List<UsersDTO> update(@PathVariable Long id, @RequestBody Users user){
+		return usersService.update(id, user);
+	}
+	
+	@DeleteMapping
+	List<UsersDTO> delete(@PathVariable Long id) {
+		return usersService.delete(id);
+	}
 	
 	@GetMapping(value = "/{id}")
 	public Optional<UsersDTO> findId(@PathVariable Long id) {
--- src/main/java/br/com/usermanagement/services/UsersService.java
+++ src/main/java/br/com/usermanagement/services/UsersService.java
@@ -1,16 +1,21 @@
 package br.com.usermanagement.services;
 
+import java.io.ObjectInputFilter.Status;
 import java.util.List;
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.usermanagement.entities.Users;
 import br.com.usermanagement.dto.UsersDTO;
 import br.com.usermanagement.repositories.UsersRepository;
-
+import br.com.usermanagement.exceptions.UnprocessableEntity;
 
 @Service
 public class UsersService {
@@ -37,6 +42,24 @@ public class UsersService {
 		return findAll();
 	}
 	
+	@Transactional(readOnly = true)
+	public List<UsersDTO> update(Long id, Users user) {
+		usersRepository.findById(id).ifPresentOrElse((existingUser) -> {
+			user.setId(id);
+			usersRepository.save(user);
+		}, () -> {
+			throw new UnprocessableEntity(HttpStatus.NOT_FOUND);
+		});
+		return findAll();
+	}
 	
+	@Transactional(readOnly = true)
+	public List<UsersDTO> delete(Long id) {
+		usersRepository.findById(id).ifPresentOrElse((existingUser) -> usersRepository.delete(existingUser), () -> {
+			throw new UnprocessableEntity(HttpStatus.NOT_FOUND);
+		});
+		
+		return findAll();
+	}
 	
 }
